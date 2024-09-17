293c2166adfaf68b1c4292c28397641c1097440a
annahico <annahico@gmail.com>
2024-09-17 12:29:40 +0200

    feat: added more options 2.py

--- Classes/17_09_24/2.py
+++ Classes/17_09_24/2.py
@@ -4,3 +4,23 @@ repeticions = int(numero)
 
 print(repeticions)
 print((frase+'\n') * repeticions)
+# \n es igual al numero = input
+
+
+# opció 2
+
+entrada = input('Escriu una frase: ')
+num_repeticions = int(input('Escriu un número baix: '))
+contador = 0
+
+while contador < num_repeticions:
+    print(entrada)
+    contador += 1
+
+
+# opció 3
+
+frase = input ('Entra Frase')+ '\n'
+repeticions = int(input('Entra repeticions'))
+
+print(frase*repeticions)
