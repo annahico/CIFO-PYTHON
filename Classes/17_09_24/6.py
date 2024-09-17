cbb160b429c62c284dd8b27a3d50aaa05bed0d37
annahico <annahico@gmail.com>
2024-09-17 13:47:04 +0200

    feat: finish exercice 6

---
+++ Classes/17_09_24/6.py
@@ -0,0 +1,11 @@
+
+cadena = "'A mi m'agrada la pizza, i també la pasta', 'pizza'"
+
+print(cadena.split(','))
+print(cadena.partition('pizza'))
+
+# entrada = input("Introdueix frase, una coma, i una paraula per partir la frase")
+
+# llista = entrada.split(',')
+
+# print(llista)
---
+++ Classes/17_09_24/Untitled-1.py
@@ -0,0 +1,5 @@
+print("hola")
+
+cadena = input()
+
+print(cadena)
