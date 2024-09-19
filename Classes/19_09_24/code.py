032e47f76687514287ebe0ff7db3eb4f6b68338b
annahico <annahico@gmail.com>
2024-09-19 12:44:26 +0200

    feat: added code in python

---
+++ Classes/19_09_24/code.py
@@ -0,0 +1,15 @@
+llista = [[1, 2.5, "hola", True],[2, 5.6, "adios", False]]
+
+for fila in llista:
+    for element in fila:
+        print(element)
+
+
+
+
+'''
+[1, 2.5, "hola", True]
+[2, 5.6, "adios", False]
+
+print(llista) #Sortida: [1, 2.5, 'hola', True]
+'''
