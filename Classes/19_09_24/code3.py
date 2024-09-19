7358ea8e4f01554ad90a52c9e3e26c27cdb66859
annahico <annahico@gmail.com>
2024-09-19 13:44:28 +0200

    feat: added code3

---
+++ Classes/19_09_24/code3.py
@@ -0,0 +1,7 @@
+lista_cadenes = ["hola", "hola","hola", "hola", "cadena1", "cadena2", "cadena3", "cadena4", "cadena5", "cadena6"]
+
+while "hola" in lista_cadenes:
+    lista_cadenes.pop(8)
+
+    
+# les posicions comencen amb el 0, NO amb el 1.
