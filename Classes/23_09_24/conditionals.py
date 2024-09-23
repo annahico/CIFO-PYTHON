0e1bb9843227a3eeb82f3f57ab837cc37c0687d9
annahico <annahico@gmail.com>
2024-09-23 10:45:18 +0200

    feat: added new conditionals with regex

---
+++ Classes/23_09_24/conditionals.py
@@ -0,0 +1,10 @@
+numero = int(input("Introdueix el número:  "))
+
+if numero > 0:
+    print(f"El número {numero} és Positiu")
+
+elif numero < 0:
+    print(f"El número {numero} és Negatiu")
+
+else:
+    print(f"El número {numero} és Zero.")
---
+++ Classes/23_09_24/conditionals2.py
@@ -0,0 +1,19 @@
+import re
+
+# Corrected pattern: 'compile', and match numbers with optional negative sign
+patro = re.compile(r"(?<!\S)-?[0-9]+(?!\S)") #(r"-?[0-9]+")
+
+entrada = input("Escriu un número: ")
+
+# Find all numbers in the input
+print(patro.findall(entrada))
+
+# Check if the input is zero, positive or negative
+if entrada == "0":
+    print(f"El número que has escrit és {entrada} i té un valor de 0")
+
+elif entrada.isdigit():
+    print(f"El nombre {entrada} és positiu")
+
+elif entrada.startswith('-') and entrada[1:].isdigit():
+    print(f"El nombre {entrada} és negatiu")
