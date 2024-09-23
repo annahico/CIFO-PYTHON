9f25f3164093abf9ad6ee6215fbc5da5bbf42f18
annahico <annahico@gmail.com>
2024-09-23 12:43:40 +0200

    update: updated conditionals4

--- Classes/23_09_24/conditionals4.py
+++ Classes/23_09_24/conditionals4.py
@@ -1,15 +1,17 @@
-while True: # aixi no utilitzem el regex
+while True: # aixi no utilitzem el regex, es un WHILE infinito
     
     try:
         numero = int(input("Escriu un número: "))  # entra en bucle fins que sigui correcte
         break  # quan es correcte el número trenca el bucle
-    except ValueError:
+    except ValueError: # si no sabem quin tipus d'error tenim, NOMES posem except
         print("Entrada Errònia, torna-hi")  # torna a demanar el número si es erroni
 
 # Condicions per determinar el signe del númeror
 if numero == 0:
     print(f"El número que has escrit és {numero} i té un valor de 0.")
+
 elif numero > 0:
     print(f"El nombre {numero} és positiu.")
+
 else:
     print(f"El nombre {numero} és negatiu.")
