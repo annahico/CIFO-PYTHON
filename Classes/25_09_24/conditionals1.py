3d80ed038773aca8f424cbe032e56140f5147623
annahico <annahico@gmail.com>
2024-09-25 09:44:12 +0200

    feat: added exercie 2 PAR

---
+++ Classes/25_09_24/conditionals1.py
@@ -0,0 +1,7 @@
+# 2. Escriure els múltiples de 2 menors o iguals a 100
+
+multiple = 1  # cada línia és un múltiple
+
+while 2 * multiple <= 100:  # els múltiples menors o iguals a 100
+    print(2 * multiple)  # aquí hem indicat que el múltiple sigui de 2 
+    multiple += 1  # sumem un múltiple cada cop
