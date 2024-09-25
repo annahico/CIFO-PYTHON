13c4a9b181cb1b96eba30e1d901234f2bb14a0c9
annahico <annahico@gmail.com>
2024-09-25 08:07:12 +0200

    feat: added comments

--- Classes/23_09_24/conditionals13.py
+++ Classes/23_09_24/conditionals13.py
@@ -1,19 +1,19 @@
 # 17. Mostrar els n primers primers17. Mostrar els n primers primers
 
-n = int(input("Introduce la cantidad de números primos a mostrar: "))
+numero = int(input("Introdueix la cantidat de números primers a mostrar: "))
 contador = 0
-num = 2
+num = 2 # Es defineix la variable num amb el valor 2, que és el primer número primer. Aquesta variable s'incrementarà per provar els següents números si són primers.
 
-while contador < n:
-    es_primer = True
-    i = 2
-    while i <= num // 2:
-        if num % i == 0:
-            es_primer = False
+while contador < numero: # Comença un bucle while que continuarà fins que s'hagin trobat I mostrat NUMEROS números primers.
+    es_primer = True # S'assumeix inicialment que NUM és un número primer
+    i = 2 # Aquesta variable s'utilitza per comprovar si NUM té algun divisor.
+    while i <= num // 2: # Aquest bucle while comprova si NUM té divisors entre 2 i la seva meitat (num // 2).
+        if num % i == 0: # Comprova si NUM és divisible per i.
+            es_primer = False # Si es troba un divisor s'indica que no es primer
             break
-        i += 1
-    if es_primer:
+        i += 1 # Incrementa el valor de i en 1 per provar el següent número com a divisor de num en la següent iteració.
+    if es_primer: #  despres de sortir del bucle, es comprova si no s'ha trobat cap divisor.
         print(num, end=' ')
-        contador += 1
-    num += 1
+        contador += 1 # Incrementa el contador perquè s'ha trobat un número primer més.
+    num += 1 # Incrementa num per provar el següent número en la propera iteració.
 print()
--- Classes/23_09_24/conditionals14.py
+++ Classes/23_09_24/conditionals14.py
@@ -1,15 +1,15 @@
 # 19. Calcular el factorial d'un número sencer positiu
 
-n = int(input("Introduce un número: "))
-factorial = 1
+numero = int(input("Introdueix un número: "))
+factorial = 1 # S’inicialitza la variable factorial amb el valor 1. Aquesta variable s’utilitzarà per calcular el factorial del número.
 i = 1
 
-if n == 0:
-    factorial = 1
+if numero == 0: # Comprova si el número introduït és 0
+    factorial = 1 # Si el número és 0, el programa assigna 1 a factorial (ja que 0! = 1).
 else:
-    while i <= n:
-        factorial *= i
-        i += 1
+    while i <= numero: # Inicia un bucle while que s'executa mentre i sigui menor o igual al número introduït.
+        factorial *= i # Es va acumulant la multiplicació de tots els nombres des de 1 fins a numero.
+        i += 1 # Incrementa i en 1 per continuar amb el següent valor en la propera iteració.
 
-print(f"El factorial de {n} es: {factorial}")
+print(f"El factorial de {numero} es: {factorial}")
 
