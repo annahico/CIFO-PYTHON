079b2b034d46881aad877ba048e45b0eb4697fae
annahico <annahico@gmail.com>
2024-09-26 13:21:37 +0200

    fix: posada la funció en exercici1

--- Classes/25_09_24/Llistes1.py
+++ Classes/25_09_24/Llistes1.py
@@ -1,11 +1,11 @@
 # 1. Funció que pren com a paràmetre una llista de números i retorna una altra llista ordenada.
 
-llista = [4,3,2,1,8]
+def ordena_llista(lista):
+    llista.sort()  
+    return llista
 
-llista.sort()
-print(llista)
+llista = [4, 3, 2, 1, 8]
+print(ordena_llista(llista))
 
-l = [5,4,3,2,1]
-
-l.sort()
-print(l)
+l = [5, 4, 3, 2, 1]
+print(ordena_llista(l))
