e12fccdf698e3e486b53105fcaaf6de7fe879075
annahico <annahico@gmail.com>
2024-09-26 09:23:56 +0200

    feat: afegit exercici 6

---
+++ Classes/26_09_24/Llistes6.py
@@ -0,0 +1,16 @@
+# 6. Funció que pren com a paràmetre una llista de números i retorna el seu valor màxim.
+
+def valor_max(llista_num):
+    return(max(llista_num)) # aqui realitzem la suma de la llista1
+llista_num = [4,3,2,1,8] # aqui s'indica el calor de la llista
+print(valor_max(llista_num))
+
+def valor_max2(llista_num2):
+    return(max(llista_num2)) # aqui realitzem la suma de la llista2
+llista_num2 = [5,4,3,2,1] # aqui s'indica el calor de la llista
+print(valor_max2(llista_num2))
+
+
+
+
+
