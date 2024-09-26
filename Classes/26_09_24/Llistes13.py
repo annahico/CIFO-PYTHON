800671c5bec91b9bf6edfc2fd8dc21cf6896e8a3
annahico <annahico@gmail.com>
2024-09-26 10:53:15 +0200

    feat: afegit exercici 14

--- Classes/26_09_24/Llistes13.py
+++ Classes/26_09_24/Llistes13.py
@@ -1,6 +1,6 @@
 # 13. Funció que pren com a paràmetre una llista de cadenes i retorna una altra llista amb les mateixes cadenes en majúscules.
 
-def minus_llista(llista):
+def mayus_llista(llista):
     llista_resultat = []
     for i in range(len(llista)):
         llista_resultat.append((str(llista[i].upper()))) # funció de cadenes, hem de convertir la llista i el I en cadena
@@ -8,4 +8,4 @@ def minus_llista(llista):
 
 llista = ['hola','com','estas']
 
-print(minus_llista(llista))
+print(mayus_llista(llista))
---
+++ Classes/26_09_24/Llistes14.py
@@ -0,0 +1,6 @@
+# 14. Funció que pren com a paràmetre una llista de cadenes i retorna una única cadena amb tots els elements concatenats (Suma d'strings o cadenes).
+
+
+llista = ['hola', 'com', 'estas']
+resultat = ''.join(llista) # no es deixa separació entre les comes perque estigui tot junt
+print(resultat) 
