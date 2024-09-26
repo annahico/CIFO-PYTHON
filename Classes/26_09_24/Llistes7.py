d05e572803e4bbbabd09562d94c4e7390586b9ba
annahico <annahico@gmail.com>
2024-09-26 09:32:26 +0200

    feat: afegit exercici 7

---
+++ Classes/26_09_24/Llistes7.py
@@ -0,0 +1,11 @@
+# 7. Funció que pren com a paràmetre una llista de números i retorna la llista en ordre invertit.
+
+llista = [4,3,2,1,8]
+
+llista.reverse()
+print(llista)
+
+l = [5,4,3,2,1]
+
+l.reverse()
+print(l)
