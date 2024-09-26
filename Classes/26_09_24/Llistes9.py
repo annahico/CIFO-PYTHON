bac4b8a81ff7f22ab38271e3d015524f9035dbb2
annahico <annahico@gmail.com>
2024-09-26 09:53:31 +0200

    feat: afegit exercici 9

---
+++ Classes/26_09_24/Llistes9.py
@@ -0,0 +1,15 @@
+# 9. Funció que pren com a paràmetre una llista de números i retorna una altra llista amb els elements en posició senar.
+
+def posicio_senar_llista(llista): # defineixo la funció
+    llista_resultat = [] 
+    for i in range(len(llista)): # El codi recorre cada índex de la llista llista (i= index)
+        if i % 2 != 0: #  El  codi recorre cada element de la llista llista I verifica si és senar (i % 2 != 0), és a dir, si no és divisible per 2.
+            llista_resultat.append(llista[i]) # Afegeix l'element llista[i] a llista_resultat.
+    
+    return llista_resultat
+
+llista1 = [4,3,2,1,8] 
+llista2 = [5,4,3,2,1] 
+
+print(posicio_senar_llista(llista1))
+print(posicio_senar_llista(llista2))
