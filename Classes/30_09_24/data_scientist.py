7cb64c724c91700068486bcc16e94c65bcb61947
annahico <annahico@gmail.com>
2024-09-30 12:33:07 +0200

    feat: afegit exercici data scientist versió 2

--- Classes/30_09_24/data_scientist.py
+++ Classes/30_09_24/data_scientist.py
@@ -1,6 +1,6 @@
 # CSV = FES UN CODI INCREMENTI EN 1 TOTS ELS NÚMEROS DE LA SEGÜENTS LLISTES
 
-# llista = [[2, 5, 10], [6, 7, 9], [4, 5, 10]]
+# matriu = [[2, 5, 10], [6, 7, 9], [4, 5, 10]]
 # |
 # codi
 # |
@@ -8,18 +8,30 @@
 # resultat = [[3, 6, 11], [7, 8, 10], [5, 6, 11]]
 
 
-def incremento(lista):
-    resultado = []  # valor de columna
-    for columna in lista:
-        resultado.append(columna + 1)
+# VERSIÓ 1
 
-    return resultado
+# def incremento(llista):
+#     resultado = []  # valor de columna
+#     for columna in llista:
+#         resultado.append(columna + 1)
 
+#     return resultado
+
+
+# matriu = [[2, 5, 10], [6, 7, 9], [4, 5, 10]]
+# resultat = []
+# for fila in matriu:  # valor de fila
+#     resultat.append(incremento(fila))
+#     incremento(fila)
+
+# print(resultat)
+
+# VERSIÓ 2
 
 matriu = [[2, 5, 10], [6, 7, 9], [4, 5, 10]]
-resultat = []
-for fila in matriu:  # valor de fila
-    resultat.append(incremento(fila))
-    incremento(fila)
-
-print(resultat)
+for element in matriu:  # element es [2,5,10]etc... de la MATRIU
+    # analitzes totes les POSICIONS de cada ELEMENT(0,1,2, etc)
+    for pos in range(len(element)):
+        # analitzes la pocisio de cada element i sumes +1 a cada element
+        element[pos] += 1
+print(matriu)
