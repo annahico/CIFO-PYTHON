722abe113b47296feeff8120eeb930e39897fe5b
annahico <annahico@gmail.com>
2024-10-01 20:39:08 +0200

    feat: added exercice3 Juge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici3.py
@@ -0,0 +1,17 @@
+# Feu un programa que escrigui la cadena de l’entrada en majúscules. El format de sortida es veurà en els exemples.
+
+# Entrada
+
+# Cadena d’entrada.
+
+# Sortida Cadena de sortida que és la cadena d’entrada en majúscules.
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input("")
+
+cadena = input()
+
+mayus = cadena.upper()
+
+print(f"{mayus}")
