c7edca6b889be9c7b3ba44cbdd6fbecf156f0a75
annahico <annahico@gmail.com>
2024-10-02 09:28:05 +0200

    feat: afegit enunciats dos primers exercicis

---
+++ Classes/Octubre/02_10_24/exercici1.py
@@ -0,0 +1,13 @@
+# A) llista strings
+
+["2, 3, hola, 24, 34.000, hola2 \n", "3,4, hola, 24, 54.000, hola3, \n",
+    "3,4, hola, 24, 54.000, hola3\n", "3,4, hola3"]
+
+# --> elimimar els . de milers
+# --> espais davant i darrere
+# --> eliminar \n final de cada string
+# --> split per la coma ","
+
+# B) fila de files o matrius
+
+[[2, 3, hola, 24, 34000, hola2], []]
