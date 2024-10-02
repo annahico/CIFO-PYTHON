cf289a9f757c7a7ec8f6a2ad24953176b77a5700
annahico <annahico@gmail.com>
2024-10-02 14:16:50 +0200

    feat: afegit prova Marvel vs DC

--- Classes/Octubre/02_10_24/prova_datasets.py
+++ Classes/Octubre/02_10_24/prova_datasets.py
@@ -3,10 +3,12 @@ import re
 
 patro = re.compile("[A-Za-z0-9 \\-_]+")
 
-arxiu = io.open("arxiu del PC", "r", encoding='utf-8')
+arxiu = io.open("C:\\Users\\Usuario\\Desktop\\Marvel_DC.csv",
+                "r", encoding='utf-8')
 
 llistadecadenes = arxiu.readlines()
 print(len(llistadecadenes))
 matriz = []
 for cadena in llistadecadenes:
     matriz.append(patro.findall(cadena))
+print(matriz[0])
