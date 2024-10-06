ae5a87db88d81275b65c1125e8cbe70a6b9442d6
annahico <annahico@gmail.com>
2024-10-06 21:41:35 +0200

    feat: afegit exercici 12 jutge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici12.py
@@ -0,0 +1,21 @@
+# Feu un programa que ordeni els caràcters d’una cadena en ordre creixent
+
+# Entrada
+
+# Cadena d’entrada.
+
+# Sortida Cadena d’entrada ordenada en ordre creixent.
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input("").
+
+
+# Entrada
+cadena = input("")
+
+# Ordena els caràcters en ordre creixent
+cadena_ordenada = ''.join(sorted(cadena))
+
+# Mostra la cadena ordenada
+print(cadena_ordenada)
