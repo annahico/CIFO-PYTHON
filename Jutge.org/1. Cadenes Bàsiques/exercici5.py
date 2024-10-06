c2ef1a776068ca9d6baabc79e3d3daf6447b6f45
annahico <annahico@gmail.com>
2024-10-06 18:19:57 +0200

    feat: afegit exercici 5 jutge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici5.py
@@ -0,0 +1,19 @@
+# Feu un programa que escrigui la cadena de l’entrada amb el primer caràcter en majúscula i la resta en minúscula. El format de sortida es veurà en els exemples.
+
+
+# Entrada
+
+# Cadena d’entrada.
+
+# Sortida Cadena de sortida que és la cadena d’entrada amb el primer caràcter en majúscula i la resta en minúscula.
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input("")
+
+
+# Entrada
+cadena = input("")
+
+# Sortida amb el primer caràcter en majúscula i la resta en minúscula
+print(cadena.capitalize())
