aa2eacbd7ac680d9cf91704f3878d7708051f7ec
annahico <annahico@gmail.com>
2024-10-06 19:59:21 +0200

    feat: afegit exercici 10 jutge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici10.py
@@ -0,0 +1,23 @@
+# Feu un programa que doni com a resultat una llista amb les paraules que hem entrat separades per espai a l’entrada.
+
+# Entrada
+
+# Paraules separades per espais.
+
+# Sortida Llista amb les paraules.
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input(""). A part, aquí per obtenir la cadena i el caràcter farem aquestes línies de codi:
+
+# cadena = input("") i també utilitzarem el mètode de cadenes split().
+
+
+# Entrada
+cadena = input("")
+
+# Converteix la cadena en una llista de paraules
+paraules = cadena.split()
+
+# Mostra la llista amb les paraules
+print(paraules)
