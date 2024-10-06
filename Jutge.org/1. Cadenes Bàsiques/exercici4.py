2dd360aa234d9656942e5f61a18533db2b571f0e
annahico <annahico@gmail.com>
2024-10-06 18:16:49 +0200

    feat: afegit exercici 4 jutge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici4.py
@@ -0,0 +1,18 @@
+# Feu un programa que escrigui la cadena de l’entrada en minúscules. El format de sortida es veurà en els exemples.
+
+# Entrada
+
+# Cadena d’entrada.
+
+# Sortida Cadena de sortida que és la cadena d’entrada en minúscules.
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input("")
+
+
+# Entrada
+cadena = input("")
+
+# Sortida en minúscules
+print(cadena.lower())
