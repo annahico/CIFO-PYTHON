f7823e85891f02eaed8a0c312c58ca83b8560368
annahico <annahico@gmail.com>
2024-10-06 18:33:23 +0200

    feat: afegit exercici 6 jutge

---
+++ Jutge.org/1. Cadenes Bàsiques/exercici6.py
@@ -0,0 +1,23 @@
+# Feu un programa que mostrarà la cadena en diferents tipus de capitalització Entrada
+
+# Cadena d’entrada
+
+# Sortida Es mostra un missatge amb els diferents tipus de capitalització que ens dóna Python. El format de la resposta es mostra en els exemples
+
+# Observació
+
+# Recorda que en tots els exercicis on es demana una cadena a l’entrada fem cadena = input("")
+
+
+cadena = input("")
+
+
+# Entrada
+cadena = input("")
+
+# Sortida amb diferents tipus de capitalització
+print(f"Minúscules: {cadena.lower()}")
+print(f"Majúscules: {cadena.upper()}")
+print(f"Capitalize: {cadena.capitalize()}")
+print(f"Títol: {cadena.title()}")
+print(f"Intercanvi: {cadena.swapcase()}")
