e16f7d164ebc03f9c7449864a139c12dd3795a62
annahico <annahico@gmail.com>
2024-10-07 12:38:13 +0200

    feat_ afegit exercici 3 POO

---
+++ Classes/Octubre/07_10_24/POO_3.py
@@ -0,0 +1,26 @@
+# Ejercicio 3 Escribir una clase en python llamada circulo que contenga un radio, con un método que devuelva el área y otro que devuelva el perímetro del circulo.
+
+import math
+
+class circulo:
+
+    def __init__(self,radio) -> None:
+        self.radio = radio
+    def area(self):
+        return math.pi*(self.radio)**2
+    def perimetro(self):
+        return 2*math.pi*self.radio
+    def __str__(self) -> str:
+        return f"Este es un objeto círculo con radio {self.radio}"
+    
+
+circulo1 = circulo(10)
+print(circulo1)
+print(circulo1.area())
+print(circulo1.perimetro())
+print(circulo1.radio)
+circulo1.radio = 1
+print(circulo1)
+print(circulo1.area())
+print(circulo1.perimetro())
+print(circulo1.radio)
