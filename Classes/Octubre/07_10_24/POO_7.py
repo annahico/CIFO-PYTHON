b1aa595f11c0ba915c3a5189028fd46b9de8c513
annahico <annahico@gmail.com>
2024-10-07 14:18:46 +0200

    feat: començat exercici 7 POO

---
+++ Classes/Octubre/07_10_24/POO_7.py
@@ -0,0 +1,45 @@
+# Ejercicio 7
+
+# Se requiere un programa que modele una cuenta bancaria que posee los siguientes atributos:
+
+# - Nombres del titular
+# - Apellidos del titular
+# - Numero de la cuenta bancaria
+# - Tipo de cuenta: puede ser una cuenta de ahorros o una cuenta
+# - Saldo de la cuenta
+
+# Se debe definir un constructor que inicialice los atributos de la clase.
+# Cuando se crea una cuenta bancaria, su saldo inicial tiene un valor de cero.
+# En una determinada cuenta bancaria se puede:
+# - Imprimir por pantalla los valores de los atributos de una cuenta bancaria
+# - Consultar el saldo de una cuenta bancaria
+# - Consignar un determinado valor en la cuenta bancaria, actualizando el saldo correspondiente
+# - Retirar un determinado valor de la cuenta bancaria, actualizando el saldo correspondiente. Es necesario tener en cuenta que no se puede realizar el retiro si el valor solicitado supera el saldo actual de la cuenta.
+
+
+class cuenta:
+
+    def __init__(self) -> None:
+        pass
+
+    def nombre(self):
+        pass
+
+    def apellidos(self):
+        pass
+
+    def numero(self):
+        pass
+
+    def saldo(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+    
+
+
+cuenta1 = cuenta()
+        
+        
+
