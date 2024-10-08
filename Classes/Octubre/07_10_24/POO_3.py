1bbc6b91016b909ba38dd81b043180f7a901986b
annahico <annahico@gmail.com>
2024-10-08 09:23:52 +0200

    feat: afegida correcció exercici 3 POO i reorganització exercicis

--- Classes/Octubre/07_10_24/POO_3.py
+++ Classes/Octubre/07_10_24/POO_3.py
@@ -1,21 +1,19 @@
 # Ejercicio 3 Escribir una clase en python llamada circulo que contenga un radio, con un método que devuelva el área y otro que devuelva el perímetro del circulo.
 
 import math
-
 class circulo:
-
-    def __init__(self,radio) -> None:
+    def __init__(self, radio):
         self.radio = radio
-
+        
     def area(self):
         return math.pi*(self.radio)**2
-    
+
     def perimetro(self):
         return 2*math.pi*self.radio
-    
-    def __str__(self) -> str:
-        return f"Este es un objeto círculo con radio {self.radio}"
-    
+
+    def __str__(self):
+        return f" Este es un objeto circulo con radio {self.radio}"
+
 
 circulo1 = circulo(10)
 print(circulo1)
---
+++ Classes/Octubre/08_10_24/POO_9.py
@@ -0,0 +1,16 @@
+# Ejercicio 9 
+# Sigue los pasos: 
+# • Crea  una  clase,  Triangulo.  Su  método  __init__()  debe  tomar  como  
+# argumentos self, angle1, angle2 y angle3. Asegúrate de establecerlos 
+# apropiadamente en el cuerpo del método __init__().  
+# • Crea una variable llamada numero_de_lados y ponla igual a 3.  
+# • Crea  un  método  llamado  comprobar_angulos.  La  suma  de  los  tres  
+# ángulos  de  un  triángulo  debe  devolver  True  si  la  suma  de  ángulo1,  
+# ángulo2 y ángulo3 es igual a 180, y False en caso contrario.  
+# • Crea  una  variable  llamada  mi_triangulo  y  hazla  igual  a  una  nueva  
+# instancia  de  tu  clase  Triangulo.  Pásale  tres  ángulos  que  sumen  180  
+# (por ejemplo, 90, 30, 60). 
+# • Imprime mi_triangulo.numero_de_lados e imprime 
+# mi_triangulo.comprobar_angulos().      
+
+
