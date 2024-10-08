3d6a03ef1a59a2ab98c97efa958fdfe49a66dd79
annahico <annahico@gmail.com>
2024-10-08 14:21:22 +0200

    feat: millorat exercici 8 POO

--- Classes/Octubre/08_10_24/POO_8.py
+++ Classes/Octubre/08_10_24/POO_8.py
@@ -1,10 +1,10 @@
 # Ejercicio 8
 # 1 - Crear una clase Cálculos con un constructor por defecto (sin parámetros) que permita realizar varios cálculos sobre números enteros.
 # 2 - Crear un método llamado factorial() que permita calcular el factorial de un entero.
-# 3  -  Crear  un  método  llamado  suma()  que  permita  calcular  la  suma  de  los  primeros  n  enteros 1 + 2 + 3 + .. + n.
+# 3 - Crear  un  método  llamado  suma()  que  permita  calcular  la  suma  de  los  primeros  n  enteros 1 + 2 + 3 + .. + n.
 # 4 - Crea un método llamado testPrimo() en la clase Cálculo para comprobar si un número es primo. El resultado será True o False.
 # 5 - Crear un método llamado testPrimos() que permita comprobar si dos números son primos entre sí.
-# 6 -  Cree  un  método  tablaMult()  que  cree  y  muestre  la  tabla  de  multiplicación  de  un  número  entero  dado.  A  continuación,  cree  un  método  allTablesMult()  para  mostrar  todas las tablas de multiplicación de enteros 1, 2, 3, ..., 9.
+# 6 - Cree  un  método  tablaMult()  que  cree  y  muestre  la  tabla  de  multiplicación  de  un  número  entero  dado.  A  continuación,  cree  un  método  allTablesMult()  para  mostrar  todas las tablas de multiplicación de enteros 1, 2, 3, ..., 9.
 # 7 - Crear un método listDiv() que obtenga todos los divisores de un entero dado en una nueva lista llamada Ldiv.
 
 
@@ -61,20 +61,13 @@ class Calculos:
         return "Clase para realizar cálculos diversos"
 
 
-# Ejemplo de uso
 c = Calculos()
-
-# Pruebas
 print("Factorial de 5:", c.factorial(5))         # Output: 120
 print("Suma de los primeros 5 enteros:", c.suma(5))  # Output: 15
 print("¿Es 7 un número primo?:", c.testPrimo(7))    # Output: True
 print("¿Son 15 y 28 primos entre sí?:", c.testPrimos(15, 28))  # Output: True
-
 print("\nTabla de multiplicar del 7:")
 c.tablaMult(7)
-
 print("\nTablas de multiplicar del 1 al 9:")
 c.allTablesMult()
-
-# Output: [1, 2, 4, 7, 14, 28]
 print("\nDivisores de 28:", c.listDiv(28))
